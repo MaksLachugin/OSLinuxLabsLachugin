ed113df25592d3f575bfb1d49982a7b78ffa51cc
MaksLachugin <71094259+MaksLachugin@users.noreply.github.com>
2021-12-21 21:49:55 +0300

    Create lab4_3.c

---
+++ lab4/lab4_3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[])
+{
+	int n = 0;
+	double t = 0;
+	char inp[] = "n:t::";
+	struct option longopts[] =
+	{
+		{ "number", required_argument, NULL, 'n'},
+		{ "timeout", optional_argument, NULL, 't'},
+		{}
+	};
+	while(1)
+	{
+		int c = getopt_long(argc, argv, inp, longopts, NULL);
+		if (c == -1)
+		{
+			break;
+		}
+		switch (c)
+		{
+			case 'n':
+				n=atoi(optarg);
+				break;
+			case 't':
+				if (optarg)
+				{
+					t=atoi(optarg);
+				}
+				else
+				{
+					t=1;
+				}
+				break;
+		}
+	}
+	if ((n>0) && (t>=0) && (strcmp(argv[optind-1], "--") == 0))
+	{
+		for (int i=0; i<n; i++)
+		{
+			printf("%s\n",argv[optind]);
+			sleep(t);
+		}
+	}
+	else
+	{
+		printf("usage: prntxt -n|--number <N> [-t|--timeout[<T>]] -- <TEXT>\n");
+	}
+	return 0;
+}
