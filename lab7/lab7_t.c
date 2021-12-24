cf145fa254340fb1880a5d41061c301ef57479b4
Maksim Lachugin <lachugin@cs.vsu.ru>
2021-12-24 23:40:39 +0300

    lab7_1

--- lab7/lab7_t.c
+++ lab7/lab7_t.c
@@ -2,8 +2,8 @@
 #include <sys/sem.h>
 #include <unistd.h>
 #include <stdio.h>
-struct sembuf acquire_sem = {.sem_op = -1 };
-struct sembuf release_sem = {.sem_op = 1 };
+struct sembuf start_sem = {.sem_op = -1 };
+struct sembuf finish_sem = {.sem_op = 1 };
 int main()
 {
     const int sem_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
@@ -15,25 +15,25 @@ int main()
     }
     else if (!pid) {
         while (1) {
-            semop(sem_id, &acquire_sem, 1);
+            semop(sem_id, &start_sem, 1);
             for (int i = 0; i < 10; i++) {
                 putchar('C');
                 fflush(stdout);
                 usleep(10000);
             }
             putchar('\n');
-            semop(sem_id, &release_sem, 1);
+            semop(sem_id, &finish_sem, 1);
         }
     }
     while (1) {
-        semop(sem_id, &acquire_sem, 1);
+        semop(sem_id, &start_sem, 1);
         for (int i = 0; i < 10; i++) {
             putchar('P');
             fflush(stdout);
             usleep(10000);
         }
         putchar('\n');
-        semop(sem_id, &release_sem, 1);
+        semop(sem_id, &finish_sem, 1);
     }
     return 0;
 }
